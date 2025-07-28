5b427b931db860857f89dfb5b592f509941b08d6
tayonpoli <tayonpoli@gmail.com>
2025-07-28 12:47:02 +0700

    icon

--- settings/settings.ts
+++ settings/settings.ts
@@ -1,5 +1,5 @@
 export const url = "https://rubix-documents.vercel.app"
-export const siteicon = "/icon.svg"
+export const siteicon = "/icon.png"
 
 export const gtm = "GTM-XXXXXXX"
 export const gtmconnected = true
